0862f87b5cd0ca5a15a7d4c3a7d0a0578f729311
Gulshat <ghusainova00@gmail.com>
2021-02-10 15:40:22 +0300

    js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+var money;
+var income;
+var addExpenses;
+var deposit;
+var mission;
+var period;
 alert("Любой текст");
-console.log('Любой текст');
+console.log('Любой текст в консоли');
